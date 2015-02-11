89bd000d88a8544cc35dde2a0d6aface354c235f
Sean Perryman <se@nperryman.com>
2015-02-10 18:20:47 -0800

    working on adding validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211005112) do
+ActiveRecord::Schema.define(version: 20150211020130) do
 
   create_table "patients", force: :cascade do |t|
     t.integer  "patientID",  limit: 4
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150211005112) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
